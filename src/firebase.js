b98de5d330ade48e509cbd7e1ff7309d41708cc9
Dr Garbas <dr.garbas@gmail.com>
2025-08-24 19:29:41 +0300

    Initial project setup

---
+++ src/firebase.js
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAthbIFESHt5c1hPGmmiS8qlEN6ybYzFcg",
+  authDomain: "vui-be-based-v10.firebaseapp.com",
+  projectId: "vui-be-based-v10",
+  storageBucket: "vui-be-based-v10.firebasestorage.app",
+  messagingSenderId: "795520495464",
+  appId: "1:795520495464:web:06592a00644a64d388f0dd"
+};
+
+// 3. Inicializuojame Firebase aplikaciją
+// Tai reikia padaryti tik vieną kartą visoje programoje
+const app = initializeApp(firebaseConfig);
+
+// 4. Gauname prieigą prie konkrečių Firebase paslaugų
+// Šiuos kintamuosius naudosime kituose programos failuose
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+// 5. Eksportuojame paslaugas, kad galėtume jas naudoti kitur
+export { db, auth };
